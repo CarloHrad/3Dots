98188704be6e76dc173cb108d9e51d37d5e99ea9
CarloHrad <vinicarlossouza226@gmail.com>
2025-09-04 16:59:36 -0300

    commit teste

---
+++ src/main/java/com/example/tridots/TridotsApplication.java
@@ -0,0 +1,13 @@
+package com.example.tridots;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TridotsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TridotsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tridots/TridotsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tridots;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TridotsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
